324895c5c24fb7c6bf61a2fc3d894594cc93e2be
seamusrobertmurphy <seamusrobertmurphy@gmail.com>
2024-12-25 06:46:34 -0600

    GLANCE added

---
+++ output_formats.R
@@ -0,0 +1,26 @@
+### PDF format
+
+output: 
+  pdf_document: 
+  toc: true
+toc_depth: 3
+pandoc_args: --listings
+includes:
+  in_header: preamble.tex
+
+editor_options: 
+  markdown: 
+  wrap: 80
+
+
+### WORD format
+
+output: 
+  word_document: 
+  toc: true
+toc_depth: 3
+
+editor_options: 
+  markdown: 
+  wrap: 80
+always_allow_html: true
